b509f4d19ccf820260511d3decedb932fbaef4df
Muhammad Iqbal S <yakuzuyoa@gmail.com>
2022-08-11 20:04:52 +0700

    Add form, post method, and body-parser lib

--- Backend/server.js
+++ Backend/server.js
@@ -6,3 +6,11 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log('Start Server on Port', PORT)
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/quote', (req, res) => {
+    console.log('Helooooooo')
+})
