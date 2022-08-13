e581442b28658dd21822fd752602aae118a68607
Muhammad Iqbal S <yakuzuyoa@gmail.com>
2022-08-13 22:21:20 +0700

    CRUD MySQL & Node.js Done!

--- Backend/controller.js
+++ Backend/controller.js
@@ -7,15 +7,79 @@ exports.home = (req, res) => {
     resp.connected("API Connected!", res)
 }
 
-exports.add = (req, res) => {
-    let nama = req.body.nama
-    let quote = req.body.quote
-
-    connection.query('INSERT INTO kutipan (nama, quote) VALUES (?, ?)', [nama,quote], (err, rows) => {
+exports.show = (req, res) => {
+    connection.query('SELECT * FROM kutipan', (err, rows) => {
         if (err) {
             throw err;
         } else {
-            resp.connected("Successfull Add Data", res)
+            // res.json(rows);
+            // res.end();
+            resp.connected(rows, res);
+        }
+    })
+}
+
+exports.add = (req, res) => {
+    const data = {
+        nama: req.body.nama,
+        quote: req.body.quote
+    }
+
+    connection.query(`INSERT INTO kutipan (nama, quote) VALUES (?,?)`, [data.nama, data.quote],(err) => {
+        if (err) {
+            throw err;
+        } else {
+            resp.connected("Quote Berhasil diTambahkan!", res);
+        }
+    })
+}
+
+exports.edit = (req, res) => {
+    const data = {
+        id_kutipan: req.body.id_kutipan,
+        nama: req.body.nama,
+        quote: req.body.quote
+    }
+
+    connection.query('SELECT * FROM kutipan WHERE kutipan.id_kutipan=?', data.id_kutipan, (err, rows) => {
+        if (err) {
+            throw err;
+        } else {
+            if (rows.length) {
+                connection.query('UPDATE kutipan SET nama=?, quote=? WHERE id_kutipan=?', [data.nama, data.quote, data.id_kutipan], (err) => {
+                    if (err) {
+                        throw err;
+                    } else {
+                        resp.connected("Quote Berhasil diUbah!", res);
+                    }
+                })
+            } else {
+                resp.connected("Id Tidak diTemukan!", res)
+            }
+        }
+    })
+}
+
+exports.del = (req, res) => {
+    const data = {
+        id_kutipan: req.body.id_kutipan
+    }
+
+    connection.query('SELECT * FROM kutipan WHERE kutipan.id_kutipan=?', data.id_kutipan, (err, rows) => {
+        if (err) {
+            throw err;
+        } else {
+            if (rows.length) {
+                connection.query('DELETE FROM kutipan WHERE id_kutipan=?', data.id_kutipan, (err) => {
+                    if (err) {
+                        throw err;
+                    } else {
+                        resp.connected("Quote Berhasil diHapus!", res);
+                    }
+                })
+            } else {
+                resp.connected("Id Tidak diTemukan!", res);
+            }
         }
     })
 }
--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { add } = require('./controller');
+const { add, show, home, edit, del } = require('./controller');
 const app = express();
 
 const PORT = 3000;
@@ -13,9 +13,22 @@ app.listen(PORT, () => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+    // res.sendFile(__dirname + '/index.html')
+    home(req, res)
 })
 
-app.post('/kutipan', (req, res) => {
+app.get('/kutipan', (req, res) => {
+    show(req, res);
+})
+
+app.post('/kutipan/add', (req, res) => {
     add(req, res);
+})
+
+app.post('/kutipan/edit', (req, res) => {
+    edit(req, res);
+})
+
+app.post('/kutipan/delete', (req, res) => {
+    del(req, res);
 })
