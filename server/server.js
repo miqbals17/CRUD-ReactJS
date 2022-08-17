982df2e1f8bdea09b7eefa5be0d9585a4848de0b
Muhammad Iqbal S <yakuzuyoa@gmail.com>
2022-08-17 13:27:12 +0700

    Ganti nama folder backend -> server

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { add, show, home, edit, del } = require('./controller');
+const app = express();
+
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+    console.log('Start Server on Port', PORT)
+});
+
+app.get('/', (req, res) => {
+    home(req, res)
+})
+
+app.get('/kutipan', (req, res) => {
+    show(req, res);
+})
+
+app.post('/kutipan/add', (req, res) => {
+    add(req, res);
+})
+
+app.post('/kutipan/edit', (req, res) => {
+    edit(req, res);
+})
+
+app.post('/kutipan/delete', (req, res) => {
+    del(req, res);
+})
